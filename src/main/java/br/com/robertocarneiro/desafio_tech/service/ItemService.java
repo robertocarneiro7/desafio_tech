e46339c02a037674fe290173cf6ce6352f02b5b6
roberto.carneiro <roberto.carneiro@dbccompany.com.br>
2020-12-02 08:47:24 -0300

    Optimizing imports

--- src/main/java/br/com/robertocarneiro/desafio_tech/service/ItemService.java
+++ src/main/java/br/com/robertocarneiro/desafio_tech/service/ItemService.java
@@ -1,7 +1,6 @@
 package br.com.robertocarneiro.desafio_tech.service;
 
 import br.com.robertocarneiro.desafio_tech.dto.Item;
-import br.com.robertocarneiro.desafio_tech.dto.Sale;
 
 import java.util.List;
 
